00675974a16f218145db542d223c85f8c595df9f
jsantman29 <nacljohn2@pm.me>
2020-10-21 22:16:50 -0400

    Updated refresh-sites script.

--- refresh-sites.sh
+++ refresh-sites.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 VPS="jsantman29@jsantman29.me"
 VPS_DIR="~/sites"
-export CURRENT_VPS=${VPS}
 
 # Downloads all the images and updates the containers with the new ones.
 SCRIPT="cd ${VPS_DIR}/; docker-compose down; docker-compose up -d --remove-orphans; docker image prune;"

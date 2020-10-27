a5aa3029eb138f35d9c86ecd9e186afa66d2e512
jsantman29 <nacljohn2@pm.me>
2020-10-27 13:11:59 -0400

    Updated nginx.conf so that Vocabulary Expander can be live.

--- update-configs.sh
+++ update-configs.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 VPS="jsantman29@jsantman29.me"
 
-export CURRENT_VPS=${VPS}
 echo "Updating README.md..."
-scp README.md ${CURRENT_VPS}:~/sites/.
+scp README.md ${VPS}:~/sites/.
 
 echo "Updating docker-compose.yml..."
-scp docker-compose.yml ${CURRENT_VPS}:~/sites/.
+scp docker-compose.yml ${VPS}:~/sites/.
 
 echo "Updating nginx.conf..."
-scp nginx.conf ${CURRENT_VPS}:~/sites/.
+scp nginx.conf ${VPS}:~/sites/.
